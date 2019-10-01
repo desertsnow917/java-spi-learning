eee74f01d84d62bea23fe285d97b728ebaa059e8
desertsnow917 <466131874@qq.com>
2019-10-01 23:05:37 +0800

    Add the run result of the test.

--- src/test/java/cn/lovelimei/JavaSPITest.java
+++ src/test/java/cn/lovelimei/JavaSPITest.java
@@ -8,6 +8,11 @@ import java.util.ServiceLoader;
  * Created by DesertSnow on 2019/10/1 22:39.
  */
 public class JavaSPITest {
+    /**
+     * Java SPI
+     * Hello, I am Optimus Prime.
+     * Hello, I am Bumblebee.
+     */
     @Test
     public void sayHello() throws Exception {
         ServiceLoader<Robot> serviceLoader = ServiceLoader.load(Robot.class);
